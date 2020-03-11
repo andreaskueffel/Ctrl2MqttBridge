80f743e6055d208f86ea5218e7fa1c8d4b2ffa40
Andreas Küffel <andreas.kueffel@praewema.de>
2020-03-11 10:46:56 +0100

    Added stub project for common client code

---
+++ CtrlConnection/BridgeConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttBridge
+{
+    public class BridgeConnection
+    {
+        //Stub class to integrate common Code for client side
+    }
+}
